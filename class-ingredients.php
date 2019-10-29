189cbc5716681fffe80a740bcc27c8873f9a6740
kaptan200496 <evgeniykaptan@gmail.com>
2019-10-29 14:01:34 +0200

    добавил 2 sql файла для описания создания таблиц для выполнения кода на двух таблицах и возможного выполнения на трёх , добавил классы ингридиентов и рецептов

---
+++ class-ingredients.php
@@ -0,0 +1,94 @@
+<?php 
+class Ingredients {
+	// id Ингридиента в базе
+	// integer
+	public $id;
+	// id рецепта в базе, к которому относится ингридиент
+	// integer
+	public $recipe;
+	// Название ингридиента
+	// string
+	public $name;
+	// Количество данного ингридиента
+	// string
+	public $amount;
+
+	function __construct($object) {
+		// Если передается объект с полем id , то записываем его 
+		if(isset($object->id)) {
+			$this->id = intval($object->id);
+		}
+		// Записываем название рецепта в поле name, предварительно проверяя его через real_escape_string
+		$this->name = Database::sanitizeString($object->name);
+		// Получаем из переданного объекта id  и по нему, с помощью класса Recipe достаем объект рецепта
+		$this->recipe = Recipe::getById($object->recipe);
+		// Получаем из переданного объекта количество ингридиента
+		$this->amount = Database::sanitizeString($object->amount);
+
+	}
+	public function saveToDB() {
+		$selectEx = "SELECT * FROM ingredients WHERE name = '{$this->name}' and recipe = '{$this->recipe}'";
+		$responseDB = Database::query($selectEx);
+		if($responseDB->num_rows == 0) {
+			$insertEx = "INSERT INTO ingredients (
+				recipe,
+				name,
+				amount
+			) VALUES (
+				'{$this->recipe}',
+				'{$this->name}',
+				'{$this->amount}'
+			)";
+			Database::query($insertEx);
+		}
+		else {
+			print "Ингридиент уже добавлен.";
+		}
+	}
+
+	public static function getIngByRecipe($recipeId) {
+		$r_id = intval($recipeId);
+		$selectEx = "SELECT * FROM ingredients WHERE recipe = {$r_id}";
+		$responseDB = Database::query($selectEx);
+		if($responseDB->num_rows > 1) {
+			$arrayOfIngredients = [];
+			for($i = 0; $i < $responseDB->num_rows; $i++) {
+				$responseRow = $responseDB[$i]->fetch_assoc();
+				$id = intval($responseRow['id']);
+				$name = $responseRow['name'];
+				$recipe = intval($responseRow['recipe'];
+				$amount = $responseRow['amount'];
+				$object = (object)[
+					'id' => $id,
+					'name' => $name,
+					'recipe' => $recipe,
+					'amount' => $amount
+				];
+				array_push($arrayOfIngredients, $object);
+			}
+			return $arrayOfIngredients;
+		}
+		else {
+			print "Ингредиенты не найдены.";
+		}
+	}
+
+	public static function ingChange($ingFdb, $recipe, $ingFupd, $amount) {
+		$recipe_id = intval($recipe);
+		$updtName = Database::sanitizeString($ingFupd);
+		$updtAmount = Database::sanitizeString($amount);
+		$ing_name = Database::sanitizeString($ingFdb);
+		$selectEx = "SELECT * FROM ingredients 
+		WHERE recipe = {$recipe_id} and name = '{$ing_name}'";
+		$responseDB = Database::query($selectEx);
+		if($responseDB->num_rows == 1) {
+			$updateEx = "UPDATE ingredients SET name = '{$updtName}', 
+			amount = '{$updtAmount}'";
+			Database::query($updateEx);
+		}
+		else {
+			print "Ингредиент не найден.";
+		}
+	}
+}
+?>
---
+++ class-recipe.php
@@ -0,0 +1,100 @@
+<?php
+class Recipe {
+	// id в базе данных
+	// integer
+	public $id;
+
+	// Название рецепта
+	// string
+	public $name;
+
+	// Описание рецепта
+	// Optional. String.
+	public $description;
+
+	// Конструктор объекта 
+	function __construct($object) {
+		// Если передается объект с полем id , то записываем его 
+		if(isset($object->id)) {
+			$this->id = intval($object->id);
+		}
+		// Записываем название рецепта в поле name, предварительно проверяя его через real_escape_string
+		$this->name = Database::sanitizeString($object->name);
+		// Если передаваемый объект содержит поле описания , то записываем его
+		if(isset($object->description)) {
+			$this->description = Database::sanitizeString($object->description);
+		}
+	}
+
+	public  function saveToDB() {
+		// записываем в переменную выражение для проверки сохраняемого рецепта в базе
+		$selectEx = "SELECT * FROM recipe WHERE name = '{$this->name}'";
+		$responseDB = Database::query($selectEx);
+		// Если в базе нет записей об этом рецепте, то записываем его в базу.
+		if($responseDB->num_rows == 0) {
+			$insertEx = "INSERT INTO recipe (
+				name,
+				description
+			) VALUES (
+				'{$this->name}',
+				'{$this->description}'
+			)";
+			Database::query($insertEx);
+		}
+		// Если в базе есть такой рецепт, то отвечаем что рецепт уже есть
+		else {
+			print "Рецепт уже записан";
+		}
+	}
+
+	public static function getByName($recipe) {
+		// Очищаем поступившие данные для безопасности нашей БД
+		$clean_name = Database::sanitizeString($recipe);
+		// Создаем выражение для вытягивания из базы рецепта
+		$selectEx = "SELECT * FROM recipe WHERE name = '{$clean_name}'";
+		$response = Database::query($selectEx);
+		// Если рецепт найден , то отдаем его , если нет , то отвечаем что рецепта в базе нет
+		if($response->num_rows == 1) {
+			$responseRow = $response->fetch_assoc();
+			$id = $responseRow['id'];
+			$name = $responseRow['name'];
+			$description = $responseRow['description'];
+			$recipeObject = (object)[
+				"id" => $id,
+				"name" => $name,
+				"description" => $description
+			];
+			$returnObject = new Recipe($recipeObject);
+			return $returnObject;
+		}
+		else {
+			print "Рецепт не найден.";
+		}
+	}
+
+		public static function getById($id) {
+		// Очищаем поступившие данные для безопасности нашей БД
+		$clean_id = intval($id);
+		// Создаем выражение для вытягивания из базы рецепта
+		$selectEx = "SELECT * FROM recipe WHERE name = '{$clean_id}'";
+		$response = Database::query($selectEx);
+		// Если рецепт найден , то отдаем его , если нет , то отвечаем что рецепта в базе нет
+		if($response->num_rows == 1) {
+			$responseRow = $response->fetch_assoc();
+			$id = $responseRow['id'];
+			$name = $responseRow['name'];
+			$description = $responseRow['description'];
+			$recipeObject = (object)[
+				"id" => $id,
+				"name" => $name,
+				"description" => $description
+			];
+			$returnObject = new Recipe($recipeObject);
+			return $returnObject;
+		}
+		else {
+			print "Рецепт не найден.";
+		}
+	}
+}
+?>
